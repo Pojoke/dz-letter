8569359db510f17e9987a82fce87347f7c806208
Pojoke <pavlykdmutro@gmail.com>
2024-11-01 15:48:44 +0200

    dz2

--- 7_11/footer.php
+++ 7_11/footer.php
@@ -7,5 +7,6 @@
 
 <!-- Підключаємо Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="style.css">
 </body>
 </html>
--- 7_11/nav.php
+++ 7_11/nav.php
@@ -4,25 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+   
     <!-- Header і навігація -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Мій Сайт</a>
+        <a class="navbar-brand" href="index.php">Мій Сайт</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.html">Головна</a>
+                    <a class="nav-link active" href="index.php">Головна</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="about.html">Про нас</a>
+                    <a class="nav-link" href="about.php">Про нас</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contacts.html">Контакти</a>
+                    <a class="nav-link" href="contacts.php">Контакти</a>
                 </li>
             </ul>
         </div>
