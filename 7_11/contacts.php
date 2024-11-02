eb5f90bd36ac4fd95a105b062b2f6ebf50eb8352
Pojoke <pavlykdmutro@gmail.com>
2024-11-02 12:23:51 +0200

    dz

--- 7_11/contacts.php
+++ 7_11/contacts.php
@@ -3,6 +3,7 @@
 include "nav.php";
 ?>
 <!-- Основний контент -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <div class="container">
     <div class="row">
         <div class="col-lg-8 mx-auto text-center">
