38e440cadbe3ec021f8de68b8f21fa2e08a3de2f
Pojoke <pavlykdmutro@gmail.com>
2024-11-02 10:28:01 +0200

    dz

--- 7_10/biblioteka.php
+++ 7_10/biblioteka.php
@@ -10,5 +10,16 @@ function reverseString($str) {
     return strrev($str);
 }
 
+function removeEverySecondWord($text) {
+    $words = explode(" ", $text);
+    $result = [];
 
+    foreach ($words as $index => $word) {
+        if ($index % 2 == 0) {
+            $result[] = $word;
+        }
+    }
+
+    return implode(" ", $result);
+}
 // echo $str_reversed;
--- 7_10/viprob_biblioteka.php
+++ 7_10/viprob_biblioteka.php
@@ -3,3 +3,8 @@ require_once "biblioteka.php";
 $str  = "Goodmorning";
 $str_reversed = reverseString($str);
 echo $str_reversed;
+
+$text = "dad and mom and sister";
+$result_text = removeEverySecondWord($text);
+
+echo $result_text;
